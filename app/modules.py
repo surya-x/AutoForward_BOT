3f3b46ccc5225c2b96447134fc737cd4780ba395
surya-x <kumarsurya.2001@gmail.com>
2020-09-10 03:04:23 +0530

    failing to click on the chat in which unread is pre.

--- app/modules.py
+++ app/modules.py
@@ -7,16 +7,20 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from time import sleep
 from random import randint
-import xlrd
+from datetime import datetime
+
+import os
 import logging
 
 
 def connecting_with_whatsapp():
     logging.info("Connecting with whatsapp")
-    # options = Options()
+    options = Options()
 
     # options.add_argument(
     #      r"user-data-dir=C:\Users\chief_surya01\AppData\Local\Google\Chrome\User Data")
+    options.add_argument(
+         r"user-data-dir=/home/surya/.config/google-chrome")
 
     driver = webdriver.Chrome()
 
@@ -41,19 +45,94 @@ def connecting_with_whatsapp():
     return driver
 
 
-def check_unread_from_these(driver, employee, client):
+def check_unread_from_these(driver, directory, employee, client):
     logging.info("Check unread msgs for these called : ")
 
     allcontacts = driver.find_elements_by_xpath(
         ''' //*[@id="pane-side"]/div[1]/div/div/div''')
 
+    i = 1
     for each in allcontacts:
         stringlist = each.text.split("\n")
         name = stringlist[0]
         code = name.split()[0]
 
-        if code == employee or code == client:
-            if len(stringlist) > 3:
-                if stringlist[3] != ":" or stringlist[3] != ": ":
-                    driver.save_screenshot(r"/app/screenshots/")
+        if code == employee or code == client:                                            # prefix matches with the name
+            if len(stringlist) > 3:                                                       # unread msgs present
+                if stringlist[3] != ":" or stringlist[3] != ": ":                         # if not muted chat
 
+                    click_xpath = ''' //*[@id="pane-side"]/div[1]/div/div/div[''' + str(i) + "]"
+                    driver.find_element_by_xpath(click_xpath).click()
+
+                    filename = directory + str(datetime.now()) + ".png"
+                    sleep(randint(3, 6))
+                    driver.save_screenshot(filename)                                      # saving the ss
+                    sleep(randint(2, 5))
+
+                    logging.info("screenshot taken for contact " + name)
+        i += 1
+
+
+# This method is used to type the parameter given to function into search
+# bar of Whatsapp and will open the chat
+def search_bar(driver, parameter):
+    logging.info("search_bar called: ")
+    try:
+        search = driver.find_element_by_xpath(
+            '''//*[@id="side"]/div[1]/div/label/div/div[2]''')
+        search.clear()
+        search.send_keys(parameter, Keys.RETURN)
+        # sleep is used to delay the scripts for 2-4 seconds
+        sleep(randint(2, 5))
+
+    except NoSuchElementException as e:
+        logging.error("NoSuchElementException found in search_bar")
+        logging.error(e)
+    except Exception as e:
+        logging.error("Exception found in search_bar")
+        logging.error(e)
+
+
+# This method is used to send the image to a chat which is already opened.
+def send_img(driver, imgpath):
+    logging.info("Sending image to open chats: ")
+    try:
+
+        # sleep is used to delay the scripts for 1-3 seconds
+        sleep(randint(1, 4))
+        driver.find_element_by_css_selector("span[data-icon = 'clip']").click()
+
+        # sleep is used to delay the scripts for 1-3 seconds
+        sleep(randint(1, 4))
+        driver.find_element_by_css_selector(
+            "input[type='file']").send_keys(imgpath)
+
+        send = WebDriverWait(driver, 120).until(
+            EC.presence_of_element_located(
+                (By.CSS_SELECTOR, "span[data-icon='send']"))
+        )
+        # sleep is used to delay the scripts for 1-2 seconds
+        sleep(randint(1, 3))
+        send.click()
+        logging.info("Image sent!!!\n")
+    except NoSuchElementException as e:
+        logging.error("NoSuchElementException found in send_img")
+        logging.error(e)
+    except Exception as e:
+        logging.error("Exception found in send_img")
+        logging.error(e)
+
+
+def send_ss(driver, directory, tosend):
+    logging.info("Sending screenshot to contacts: ")
+    try:
+        search_bar(driver, tosend)
+        for each in os.listdir(directory):
+            # sleep is used to delay the scripts for 2-4 seconds
+            sleep(randint(2, 5))
+            filename = os.path.join(directory, each)
+            send_img(driver, filename)
+
+    except Exception as e:
+        logging.error("Exception found in send_ss")
+        logging.error(e)
--- main.py
+++ main.py
@@ -1,13 +1,17 @@
-import selenium
 from app.modules import *
 
 driver = connecting_with_whatsapp()
 
 # Fetching data from Excel/File:
 to_forward = r"Only us"
-employee_prefix = r"EMP"
-client_prefix = r"CLT"
+employee = r"EMP"
+client = r"CLT"
 
+ss_path = str(os.getcwd()) + r"/app/screenshots/"
 # Data Fetched...
 
-check_unread_from_these(driver, employee_prefix, client_prefix)
+check_unread_from_these(driver, ss_path, employee, client)
+
+send_ss(driver, ss_path, to_forward)
+
+sleep(randint(4, 7))
